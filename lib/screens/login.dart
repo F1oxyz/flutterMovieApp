479a64357db1924cc33ed098632c48b490b29142
f1oxyz <angelafloxz@gmail.com>
2026-02-12 11:34:10 -0600

    vuelta

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -17,7 +17,7 @@ class loginScreen extends StatelessWidget {
             children: [
               SizedBox(height: 70),
               Text(
-                'Bienvenido Prueba commit',
+                'Bienvenido',
                 style: TextStyle(
                   fontFamily: 'PlusJakartaSans',
                   fontWeight: FontWeight.w700,
